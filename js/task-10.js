97ec226a6125d90bb66add22ede36cd3bd881ef4
Max Sidorenko <fivemoshr@gmail.com>
2022-10-01 13:07:13 +0300

    Update task-10.js

---
+++ js/task-10.js
@@ -0,0 +1,33 @@
+const controlPanel = document.getElementById("controls");
+const inputField = controlPanel.firstElementChild;
+const createBtn = controlPanel.querySelector("button[data-create]");
+const destroyBtn = controlPanel.querySelector("button[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+    let incrementValue = 0;
+    amount = inputField.value;
+
+    for (let i = 0; i < amount; i++) {
+        const div = document.createElement("div");
+        div.style.width = 30 + incrementValue + "px";
+        div.style.height = 30 + incrementValue + "px";
+        div.style.backgroundColor = getRandomHexColor();
+        incrementValue += 10;
+        boxesContainer.append(div);
+    }
+};
+
+function destroyBoxes(event) {
+    boxesContainer.innerHTML = "";
+    inputField.value = "";
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
